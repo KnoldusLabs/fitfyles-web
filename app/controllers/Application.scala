09e162be5caec213912b9ce9d5c32ebfe144804d
Harsh Sharma <harsh@knoldus.com>
2015-10-06 15:05:16 +0530

    Introduced the mailgun

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,36 +1,35 @@
 package controllers
 
-import play.api._
-import play.api.mvc._
-import java.util.concurrent.TimeoutException
-import scala.concurrent.ExecutionContext.Implicits.global
-import play.api.Logger
-import play.api.Play
-import play.api.Play.current
-import play.api.Play.current
-import play.api.i18n.Messages
-import play.api.cache.Cache
-import play.api.data.Form
-import play.api.data.Forms._
-import play.api.mvc.Action
-import play.api.mvc.Controller
-import play.api.mvc.Security
-import java.util.concurrent.TimeoutException
-import play.api.mvc.Cookie
-import scala.concurrent.Future
-import javax.naming.ldap.Control
-import play.api.routing.JavaScriptReverseRouter
-import play.api.Routes
+import javax.inject.Inject
 
-class Application extends Controller {
+import models.MailgunServiceApi
+import play.api.libs.json.Json
+import play.api.mvc.{Action, Controller}
+import play.api.routing.JavaScriptReverseRouter
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class Application @Inject()(mailgunServiceApi: MailgunServiceApi) extends Controller {
 
   def index = Action { implicit request =>
     Ok(views.html.index("Your new application is ready."))
   }
-  
+
+  /**
+   * Sends,
+   * The account confirmation mail to user
+   * The details of new user to fitFyles account
+   * @param name
+   * @param email
+   * @return
+   */
+  def sendMail(name: String, email: String): Action[play.api.mvc.AnyContent] = Action.async { implicit request =>
+    mailgunServiceApi.sendMail(name, email) map { response =>
+      Ok(Json.obj("status" -> response.statusText))
+    }
+  }
+
   def javascriptRoutes: Action[play.api.mvc.AnyContent] = Action { implicit request =>
-    
-    Ok(JavaScriptReverseRouter("jsRoutes")(routes.javascript.Application.index)).as("text/javascript")
+    Ok(JavaScriptReverseRouter("jsRoutes")(routes.javascript.Application.sendMail)).as("text/javascript")
   }
 
 }
---
+++ app/models/MailgunServiceApi.scala
@@ -0,0 +1,62 @@
+package models
+
+import com.google.inject.ImplementedBy
+import play.api.Play
+import play.api.libs.ws._
+import scala.concurrent.Future
+import play.api.Play.current
+
+/**
+ * Created by knoldus on 6/10/15.
+ */
+@ImplementedBy(classOf[MailgunService])
+trait MailgunServiceApi {
+  def sendMail(name: String, email: String): Future[WSResponse]
+}
+
+class MailgunService extends MailgunServiceApi {
+
+  /**
+   * Mailgun auth settings
+   */
+  val mailgunAuth = WS.url(Play.current.configuration.getString("sendBox_url").get)
+    .withAuth("api", Play.current.configuration.getString("api_key").get, WSAuthScheme.BASIC)
+
+  /**
+   * Sends the account confirmation mail to user
+   * @param name
+   * @param email
+   * @return
+   */
+  def sendMail(name: String, email: String): Future[WSResponse] = {
+    mailgunAuth.post(
+      Map(
+        "from" -> Seq(Play.current.configuration.getString("from").get),
+        "to" -> Seq(email),
+        "subject" -> Seq(Play.current.configuration.getString("subject").get),
+        "html" -> Seq(name + ",",
+          "<br>", Play.current.configuration.getString("message").get)
+      )
+    )
+    sendFitFylesNotification(name, email)
+  }
+
+  /**
+   * Sends the details of new user to fitFyles account
+   * @param name
+   * @param email
+   * @return
+   */
+  private def sendFitFylesNotification(name: String, email: String): Future[WSResponse] = {
+    mailgunAuth.post(
+      Map(
+        "from" -> Seq(Play.current.configuration.getString("from").get),
+        "to" -> Seq(Play.current.configuration.getString("fitfylesEmail").get),
+        "subject" -> Seq(Play.current.configuration.getString("subject").get),
+        "html" -> Seq(Play.current.configuration.getString("notificationAlert").get,
+          "<br>", "Name : " + name,
+          "<br>", "Email : " + email)
+      )
+    )
+  }
+}
